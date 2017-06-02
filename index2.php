84583f324dc896e246168dc78461228bd6c0a4e6
Jeffrey Cooper <jcooper37@cnm.edu>
2017-06-02 09:45:26 -0600

    test code

---
+++ index2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+		<!-- custom CSS stylesheet -->
+		<link rel="stylesheet" href="css/style2.css" type="text/css" />
+
+		<title>CSS Test</title>
+	</head>
+	<body>
+		<div class="box">This is some content in a fancy box!</div>
+	</body>
---
+++ index3.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<link rel="stylesheet" href="css/style2.css" type="text/css" />
+		<title>My Kitty</title>
+	</head>
+	<body>
+		<header>
+			<h1>My Kitty Blog</h1>
+		</header>
+		<main>
+			<p>Welcome to my blog where I like to write really bad poems about my cat.</p>
+		</main>
+	</body>
+</html>
